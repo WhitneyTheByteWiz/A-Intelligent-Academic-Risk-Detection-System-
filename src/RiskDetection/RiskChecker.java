565d69cd0bca6fce9afb790329f5ea101441eebc
Malichi Josephat <MalichiJ02@users.noreply.github.com>
2026-09-10 00:49:33 +0200

    updated risk cheacker logic

---
+++ src/RiskDetection/RiskChecker.java
@@ -0,0 +1,18 @@
+public class RiskChecker {
+    public static void main(String[] args) {
+
+        String studentName = "Malichi";
+        double averageMark = 55;
+        double attendance = 68;
+
+if (attendance < 60||averageMark < 50) {
+    System.out.println(studentName + " is at high risk of failing");
+} else if (attendance < 75) {
+    System.out.println(studentName + " has moderate attendance");
+} else {
+    System.out.println(studentName + " has good attendance");
+}
+
+
+    }
+}
