1ec4c6d94a8783b71fdbdfbef83de6108e664881
Malichi Josephat <MalichiJ02@users.noreply.github.com>
2026-09-22 17:02:24 +0200

    updates wee2 risk checker

--- src/src/Risk Detection & Assessment/RiskChecker.java
+++ src/src/Risk Detection & Assessment/RiskChecker.java
@@ -1,17 +1,24 @@
-package src.RiskDetection;
+package src.riskChecker;
+
 public class RiskChecker {
+
     public static void main(String[] args) {
-        
-    String studentName = "John ";
+
+        String studentName = "John";
         double averageMark = 55;
         double attendance = 68;
 
-if (attendance < 60||averageMark < 50) {
-    System.out.println(studentName + " is at high risk of failing");
-} else if (attendance < 75) {
-    System.out.println(studentName + " has moderate attendance");
-} else {
-    System.out.println(studentName + " has good attendance");
-}
+        if (attendance < 60 || averageMark < 50) {
+
+            System.out.println(studentName + " is at high risk of failing");
+
+        } else if (attendance < 75) {
+
+            System.out.println(studentName + " has moderate attendance");
+
+        } else {
+
+            System.out.println(studentName + " has good attendance");
+        }
     }
 }
