27e0975ad13b98360feee8d66b443691c6ace7e0
Malichi Josephat <MalichiJ02@users.noreply.github.com>
2026-09-22 22:34:40 +0200

    Add methods to risk checker

--- src/src/RiskDetectionAndAssessment/RiskChecker.java
+++ src/src/RiskDetectionAndAssessment/RiskChecker.java
@@ -1,5 +1,3 @@
-package src.RiskDetectionAndAssessment;
-
 import java.util.Scanner;
 
 public class RiskChecker {
@@ -8,20 +6,32 @@ public class RiskChecker {
 
         Scanner scanner = new Scanner(System.in);
 
-        String studentName = "John";
-        double averageMark = 55;
-        double attendance = 68;
+        System.out.print("Enter student name: ");
+        String studentName = scanner.nextLine();
+
+        System.out.print("Enter average mark: ");
+        double averageMark = scanner.nextDouble();
+
+        System.out.print("Enter attendance percentage: ");
+        double attendance = scanner.nextDouble();
+
+        displayTitle();
+        checkRisk(studentName, averageMark, attendance);
+
+        scanner.close();
+    }
+
+    public static void displayTitle() {
+        System.out.println("\nACADEMIC RISK DETECTION SYSTEM");
+    }
+
+    public static void checkRisk(String studentName, double averageMark, double attendance) {
 
         if (attendance < 60 || averageMark < 50) {
-
             System.out.println(studentName + " is at high risk of failing");
-
         } else if (attendance < 75) {
-
             System.out.println(studentName + " has moderate attendance");
-
         } else {
-
             System.out.println(studentName + " has good attendance");
         }
     }
