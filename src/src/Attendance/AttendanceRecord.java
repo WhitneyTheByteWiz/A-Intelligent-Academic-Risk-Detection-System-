47b6982ef8fd9b6a7892f95424b996c85263e00a
hakamatanokuthula <hakamatanokuthula@gmail.com>
2026-09-23 09:15:18 +0200

    Made same good changes in my code

--- src/src/Attendance/AttendanceRecord.java
+++ src/src/Attendance/AttendanceRecord.java
@@ -1,5 +1,20 @@
 package src.Attendance;
 
 public class AttendanceRecord {
-    
+
+    private String studentNumber;
+    private double attendancePercentage;
+
+    public AttendanceRecord(String studentNumber, double attendancePercentage) {
+        this.studentNumber = studentNumber;
+        this.attendancePercentage = attendancePercentage;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public double getAttendancePercentage() {
+        return attendancePercentage;
+    }
 }
